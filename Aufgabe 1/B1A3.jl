8643d3ebc880815b5c9625f6143eb75c8f3b423f
SamaelGit <94474443+SamaelGit@users.noreply.github.com>
2025-04-23 10:50:31 +0200

    Update B1A3.jl

--- Aufgabe 1/B1A3.jl
+++ Aufgabe 1/B1A3.jl
@@ -1,15 +1,18 @@
 using LinearAlgebra
 
+
+#opertornorm 1 und Inf:
+
 function operatornorm(M::Matrix{Float64}, n) #n=Dimension, für 1 setze n=1, für unendlich setze n=2
     println(sum(M, dims=n))        
     return maximum(sum(M; dims=n))
 end
 
 
-
+#opertornorm F:
 
 function matrix_qaudr(M::Matrix{Float64})
-    M2 = similar(M)  # erstellt eine Matrix gleicher Größe & Typs
+    M2 = similar(M)
     for i in 1:size(M, 1), j in 1:size(M, 2)
         M2[i, j] = M[i, j]^2
     end
