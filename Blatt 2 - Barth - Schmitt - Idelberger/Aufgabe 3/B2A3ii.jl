3c2d0c11f292c2a53b36550e866b294f7afa12b5
SamaelGit <94474443+SamaelGit@users.noreply.github.com>
2025-05-01 19:13:35 +0200

    Update B2A3ii.jl

--- Blatt 2 - Barth - Schmitt - Idelberger/Aufgabe 3/B2A3ii.jl
+++ Blatt 2 - Barth - Schmitt - Idelberger/Aufgabe 3/B2A3ii.jl
@@ -1,8 +1,11 @@
+using LinearAlgebra
+
+#Aufgabe 3 i)
 
 #Vandermondematrix
 function VMatrix(x::Vector)
     n = length(x)
-    V = zeros(n,n)
+    V = zeros(n,n)                         #Startmatrix aus Nullen
     for i in 1:n, j in 1:n
         V[i,j] = x[i]^(j-1)      
     end
@@ -13,21 +16,31 @@ end
 #Determinante
 function VDet(x::Vector)
     n = length(x)
-    prod = 1
+    prod = 1                                #Produkt
     for i in 1:n, j in (i+1):n
-        prod = prod * (x[j]-x[i])
+        prod = prod * (x[j]-x[i])           #Index eines Vektors in Julia beginnt bei 1 nicht bei 0 
     end
     result = prod
 end
 
+#Aufgabe 3 ii)
+
+function Gitter(N::Int)
+    return collect(range(0, 10, N))         #collect erzuegt ein Array mit allen äquidistanten Punkten
+end
 
 
-using LinearAlgebra
 
-test=[1,2,3,4]
-println(test[2])
+#Aufgabe 3 iii)
 
-println(VMatrix(test))
-println(VDet(test))
-println(det(VMatrix(test)))
+for i in 2:10
 
+    test=Gitter(i)
+    println("Test-Vektor: ", test)
+    println("Vandermondematrix: ", VMatrix(test))
+    println("Vandermonde-Determinante: ", VDet(test))
+    println("Determinate verifizieren: ", det(VMatrix(test)))
+    println("Fehler: ", abs(det(VMatrix(test))-VDet(test)))
+    println(" ")
+
+end
